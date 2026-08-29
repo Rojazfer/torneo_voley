b3ddc122a5344bdd020d161f588eb6505b80fbfa
Rojazfer <Rojazfer@users.noreply.github.com>
2026-08-29 18:23:25 -0400

    Register users in Django admin

--- backend/cuentas/admin.py
+++ backend/cuentas/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Usuario
+
+
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    list_display = (
+        'username',
+        'email',
+        'rol',
+        'is_staff',
+        'is_active',
+        'fecha_registro',
+    )
+    list_filter = ('rol', 'is_staff', 'is_active', 'is_superuser')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informacion del torneo', {
+            'fields': ('rol', 'telefono', 'foto'),
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Informacion del torneo', {
+            'fields': ('email', 'rol', 'telefono', 'foto'),
+        }),
+    )
